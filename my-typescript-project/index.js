395141d7f5e51a71683e1faa21e318f031933277
szfa95 <szfa95@example.com>
2024-06-11 16:16:07 +0200

    h

---
+++ my-typescript-project/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// index.ts
+const hello = (name) => {
+    return `Hello, ${name}!`;
+};
+console.log(hello("World"));
---
+++ my-typescript-project/index.ts
@@ -0,0 +1,6 @@
+// index.ts
+const hello = (name: string): string => {
+    return `Hello, ${name}!`;
+  };
+  
+  console.log(hello("World"));
